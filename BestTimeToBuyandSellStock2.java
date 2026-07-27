54f90c52567ce3c6e768fdcd485e445fb850859e
Suwetha S T <suwetha346@gmail.com>
2026-07-27 21:11:22 +0530

    Create BestTimeToBuyandSellStock2.java

---
+++ BestTimeToBuyandSellStock2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<arr.length;i++)
+		{
+		    arr[i]=sc.nextInt();
+		}
+		System.out.println(maxProfit(arr));
+	}
+static int maxProfit(int arr[])
+{
+    int total = 0;
+    for(int i=1;i<arr.length;i++)
+    {
+        if(arr[i]>arr[i-1])
+        {
+            total += arr[i]-arr[i-1];
+        }
+    }
+    return total;
+}
+
+}
