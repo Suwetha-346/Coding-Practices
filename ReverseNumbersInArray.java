35680d76c69c952d3d7ba335662abeb041ea87df
Suwetha S T <suwetha346@gmail.com>
2026-07-29 00:22:38 +0530

    Update ReverseNumbersInArray.java

--- ReverseNumbersInArray.java
+++ ReverseNumbersInArray.java
@@ -1,1 +1,34 @@
+import java.io.*;
+import java.util.*;
+class Main{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+int n=sc.nextInt();
+int arr[]=new int[n];
+for(int i=0;i<n;i++)
+  {
+    arr[i]=sc.nextInt();
+  }
+  revnum(arr,n);
+}
+  static void revnum(int arr[],int n)
+  {
+    for(int i=0;i<n;i++)
+    {
+        int org = arr[i];
+        int rev = 0;
+        while(org!=0)
+        {
+            int d = org % 10;
+            rev = (rev * 10)+d;
+            org = org /10;
+        }
+        arr[i]=rev;
+    }
+    for(int i=0;i<n;i++)
+    {
+        System.out.print(arr[i]+" ");
+    }
+  }
+}
 
