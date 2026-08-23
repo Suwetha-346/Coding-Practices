abe40a1bf416d3aed2b631a5a0699ea615b007a6
Suwetha S T <suwetha346@gmail.com>
2026-08-23 16:30:05 +0530

    Update ContainsDuplicates.java

--- ContainsDuplicates.java
+++ ContainsDuplicates.java
@@ -1,1 +1,30 @@
-
+import java.io.*;
+import java.util.*;
+public class Main
+  {
+    public static void main(String[] args)
+    {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int[] arr=new int[n];
+      for(int i=0;i<arr.length;i++)
+        {
+          arr[i]=sc.nextInt();
+        }
+      System.out.println(dupl(arr));
+    }
+    public static boolean dupl(int[] arr)
+    {
+      for(int i=0;i<arr.length;i++)
+        {
+          for(int j=i+1;j<arr.length;j++)
+            {
+              if(arr[i]==arr[j])
+              {
+                return true;
+              }
+            }
+        }
+      return false;
+    }
+  }
