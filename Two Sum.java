0e12e5a3dd3a69ad1bf48282acf1d811d51c719d
Suwetha S T <suwetha346@gmail.com>
2026-08-23 16:16:00 +0530

    Update Two Sum.java

--- Two Sum.java
+++ Two Sum.java
@@ -1,1 +1,31 @@
-
+import java.io.*;
+import java.util.*;
+public class Main
+  {
+    public static void main(String[] args)
+    {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int target = sc.nextInt();
+      int[] arr=new int[n];
+      for(int i=0;i<n;i++)
+        {
+          arr[i]=sc.nextInt();
+        }
+      System.out.println(twosum(arr,target));
+    }
+    public static int[] twosum(int[] arr,int target)
+    {
+      for(int i=0;i<arr.length;i++)
+        {
+          for(int j=i+1;j<arr.length;j++)
+            {
+              if(arr[i]+arr[j]==target)
+              {
+                 return new int[]{i,j};
+              }
+            }
+        }
+      return new int[]{-1,-1};
+    }
+  }
